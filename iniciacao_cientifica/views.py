deef244c883b0ef149dc05ddb47a416763f9ec63
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-05-18 21:53:08 -0300

    AP2 finalizada

--- iniciacao_cientifica/views.py
+++ iniciacao_cientifica/views.py
@@ -3,12 +3,20 @@ from django.http import Http404
 from .models import Autor, Artigo
 
 def index(request, username):
+    pessoa = Autor.objects.filter(user=username)
+    if len(pessoa) == 0:
+        raise Http404('Arroba não encontrado')
+    artigo = Artigo.objects.filter(autores=username)
+    if len(artigo) == 0:
+        raise Http404('Autor não possui Artigos')
+    return render(request, 'iniciacao_cientifica/perfil.html', {'pessoa' : pessoa, 'artigo' : artigo})
+
+    '''
     try:
         pessoa = Autor.objects.filter(user=username)
-    except:
+    except Autor.DoesNotExist:
         raise Http404('Arroba não encontrado')
     try:
         artigo = Artigo.objects.filter(autores=username)
-    except:
-        raise Http404('Artigo não encontrado')
-    return render(request, 'iniciacao_cientifica/perfil.html', {'pessoa' : pessoa, 'artigo' : artigo})
+    except Artigo.DoesNotExist:
+        raise Http404('Autor não possui Artigos')'''   
